3841d8fde5aa90387d640a85cafb5bb188a667ff
avanetta <avanetta@prismamp.com>
2022-08-29 09:14:26 -0300

    add_cfg

--- movie-service-parcial/target/classes/movie.sql
+++ movie-service-parcial/target/classes/movie.sql
@@ -1,6 +1,6 @@
 INSERT INTO movies(name, genre, url_stream) VALUES('Dark Shadows', 'Terror', 'https://www.netflix.com/title/70217909');
 INSERT INTO movies(name, genre, url_stream) VALUES('Gremlins', 'Terror', 'https://www.netflix.com/title/562050');
-INSERT INTO movies(name, genre, url_stream) VALUES('Minions', 'Comedy', 'https://www.netflix.com/title/80033394');
+INSERT INTO movies(name, genre, url_stream) VALUES('Minions 2', 'Comedy', 'https://www.netflix.com/title/80033394');
 INSERT INTO movies(name, genre, url_stream) VALUES('World War Z', 'Terror', 'https://www.netflix.com/title/70262639');
 INSERT INTO movies(name, genre, url_stream) VALUES('Kung-fu Panda', 'Comedy', 'https://www.netflix.com/title/70075480');
 INSERT INTO movies(name, genre, url_stream) VALUES('Pacific Rim', 'Action', 'https://www.netflix.com/title/70267241');
